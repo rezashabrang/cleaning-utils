d0181ee1a131e877e66d943323018617aa488433
Reza Shabrang <rezashabrang.m@gmail.com>
2022-03-28 09:27:04 +0430

    Adding new version

--- cleaning_utils/__init__.py
+++ cleaning_utils/__init__.py
@@ -25,7 +25,7 @@ from .space_cleaner import space_cleaner
 from .to_fa_numbers import to_fa_numbers
 from .to_iso_datetime import to_iso_datetime
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = [
     "clear_stop_char",
     "replace_arabic_char",
